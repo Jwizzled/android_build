72b31d10da823d63a082bffab517954e0e794e46
MrBaNkS <banksmi09@gmail.com>
2013-11-30 06:20:12 -0600

    Revert "lazy sh"
    This reverts commit b2fdb010bf09f545f14960dcd2645a162eeda17f.

--- lazyOthers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source build/envsetup.sh
-make installclean
-# grouper
-lunch rascarlo_grouper-user && make installclean && make otapackage -j12
-# hammerhead
-lunch rascarlo_hammerhead-user && make installclean && make otapackage -j12
